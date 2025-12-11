39524bd1ee905e09f66a7e94aa354477836d6f0d
bbspon <bbspon@gmail.com>
2025-12-11 17:47:38 +0530

    updated

--- crmnext/src/services/authService.js
+++ crmnext/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api/auth"; // Use correct backend URL
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
 
 export const signup = async (username, email, mobile, password) => {
   return axios.post(`${API_BASE_URL}/signup`, {

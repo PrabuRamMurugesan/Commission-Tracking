7c0b79f3ca81cd210d83bbd8dc19874deddb67f6
bbspon <bbspon@gmail.com>
2025-12-12 16:04:04 +0530

    Updated

--- crmnext/pages/api/aiAnomalyDetectionRoutes.js
+++ crmnext/pages/api/aiAnomalyDetectionRoutes.js
@@ -1,9 +1,9 @@
-// routes/aiAnomalyDetectionRoutes.js
-const express = require("express");
-const { getAnomalyData, resolveAnomaly } = require("../controllers/aiAnomalyDetectionController");
-const router = express.Router();
+// // routes/aiAnomalyDetectionRoutes.js
+// const express = require("express");
+// const { getAnomalyData, resolveAnomaly } = require("../controllers/aiAnomalyDetectionController");
+// const router = express.Router();
 
-router.get("/", getAnomalyData);
-router.post("/resolve", resolveAnomaly);
+// router.get("/", getAnomalyData);
+// router.post("/resolve", resolveAnomaly);
 
-module.exports = router;
+// module.exports = router;
